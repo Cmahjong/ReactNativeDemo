0c25815be45e5da6dc853ebd339ad8acc9d56855
yj <1073279000@qq.com>
2018-09-19 15:15:00 +0800

    ‘4’

--- App.js
+++ App.js
@@ -21,8 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>历经4个小时终于搞定了从配置到Demo好了以就不管了</Text>
+        <Text style={styles.instructions}>帅气作者：yj</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
